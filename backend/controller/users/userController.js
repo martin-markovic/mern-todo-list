27f4b0b3ff5d990f280844fac55292122d8295e3
Martin <martin730036@gmail.com>
2024-06-12 21:37:32 +0200

    Update userController.js
    - implement login validation.
    - implement token generation.
    - add jwt as authentication payload to json responses, if requests are valid.
    - fix existing resolve issues with async calls by returning json responses.

--- backend/controller/users/userController.js
+++ backend/controller/users/userController.js
@@ -6,13 +6,13 @@ export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    res.status(400).json({ message: "Please add all fields" });
+    return res.status(400).json({ message: "Please add all fields" });
   }
 
   const userFound = await User.findOne({ email });
 
   if (userFound) {
-    res.status(400).json({ message: "User already exists" });
+    return res.status(400).json({ message: "User already exists" });
   }
 
   const salt = await bcrypt.genSalt(10);
@@ -25,20 +25,46 @@ export const registerUser = async (req, res) => {
   });
 
   if (user) {
-    res.status(201).json({
+    return res.status(201).json({
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
-    res.json({ message: "Invalid user credentials" });
+    return res.json({ message: "Invalid user credentials" });
   }
 };
 
 export const loginUser = async (req, res) => {
-  res.json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  const userFound = await User.findOne({ email });
+
+  if (userFound && (await bcrypt.compare(password, userFound.password))) {
+    return res.status(200).json({
+      _id: userFound.id,
+      name: userFound.name,
+      email: userFound.email,
+      token: generateToken(userFound._id),
+    });
+  } else {
+    return res.status(400).json({ message: "Invalid user credentials" });
+  }
+};
+
+const generateToken = (id) => {
+  return jwt.sign(
+    {
+      id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "5d",
+    }
+  );
 };
 
 export const getUser = async (req, res) => {
-  res.json({ message: "Get User Data" });
+  return res.json({ message: "Get User Data" });
 };
