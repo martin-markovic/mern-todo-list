f155f719b0f796eecf483bf5330bf619f7ce72d1
Martin <martin730036@gmail.com>
2024-06-13 23:42:48 +0200

    Update Register.jsx
    - import `useState` and `useEffect`.
    - create form for name, email, and password validation.
    - initialize state with form data.
    - add `onChange as event listener` to inputs.
    - prevent default behavior and implement `useState` functionality to `onChange`.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,76 @@
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
 function Register() {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section>
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            placeholder="Enter your name"
+            onChange={onChange}
+          ></input>
+
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={onChange}
+          ></input>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={onChange}
+          ></input>
+          <input
+            type="password"
+            id="password2"
+            name="password2"
+            value={password2}
+            placeholder="Confirm your password"
+            onChange={onChange}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Register;
