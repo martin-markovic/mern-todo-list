2a634788ad43441aa95abb58524470f34f7dbe13
Martin <martin730036@gmail.com>
2024-06-21 00:51:26 +0200

    Update GoalsList.jsx with pagination
    - integrate `useState` with default value of 1.
    - define variables `goalsPerPage`, `totalPages`, `startIndex` and `currentGoals` to compute and render goals and current page.
    - implement `previous` and `next` buttons to handle pagination.
    - integrate `handlePagination` function into buttons to handle state based on the name of the button clicked and current state.

--- frontend/src/components/GoalsList.jsx
+++ frontend/src/components/GoalsList.jsx
@@ -1,6 +1,10 @@
 import GoalItem from "./GoalItem";
+import { useState } from "react";
 
 function GoalsList({ goals, filter }) {
+  const [page, setPage] = useState(1);
+  const goalsPerPage = 5;
+
   const filteredGoals = goals.filter((goal) => {
     if (filter === "completed") {
       return goal.isCompleted === true;
@@ -12,17 +16,46 @@ function GoalsList({ goals, filter }) {
 
     return true;
   });
+
+  const totalPages = Math.ceil(filteredGoals.length / goalsPerPage);
+  const startIndex = (page - 1) * goalsPerPage;
+  const currentGoals = filteredGoals.slice(
+    startIndex,
+    startIndex + goalsPerPage
+  );
+
+  const handlePage = (e) => {
+    if (e.target.name === "previous" && page > 1) {
+      setPage(page - 1);
+    } else if (e.target.name === "next" && page < totalPages) {
+      setPage(page + 1);
+    }
+  };
+
   return (
     <section>
-      {filteredGoals.length > 0 ? (
+      {currentGoals.length > 0 ? (
         <>
-          {filteredGoals.map((goal) => (
+          {currentGoals.map((goal) => (
             <GoalItem key={goal._id} goal={goal} />
           ))}
         </>
       ) : (
         <h3>No goals, please create a goal.</h3>
       )}
+      <div className="pagination">
+        {page > 1 && (
+          <button name="previous" onClick={handlePage}>
+            Previous
+          </button>
+        )}
+        <span>{page}</span>
+        {page < totalPages && (
+          <button name="next" onClick={handlePage}>
+            Next
+          </button>
+        )}
+      </div>
     </section>
   );
 }
