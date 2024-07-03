d35ede2ef3201f6dbd60f4ac54a142adbf3d253c
Martin <martin730036@gmail.com>
2024-07-03 02:48:18 +0200

    update implement mock data
    - implement `mockDB` to test database storage and API.
    - implement `MockUSerData` to test user and goals data.
    - remove the redundant `mockRouter` and API endpoint from `mockServer.js`.
    - delete the redundant module `mockRouter.js`.

--- test/mocks/backend/mockServer.js
+++ test/mocks/backend/mockServer.js
@@ -1,12 +1,9 @@
 import express from "express";
-import mockRouter from "./routes/mockRouter.js";
 
 const mockApp = () => {
   const app = express();
 
   app.use(express.json());
-
-  app.use("/", mockRouter);
 
   return app;
 };
